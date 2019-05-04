168852257ff155c5101df42631ea02c4a77d9882
Oli Oskarsson <oli@oskarsson.nl>
2019-05-04 19:01:04 +0200

    Added initial bapp listing

--- App/Containers/BappItemAdd.js
+++ App/Containers/BappItemAdd.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   SafeAreaView, ScrollView, Image, Text,
 } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 import RoundedButton from '../Components/RoundedButton';
 
 import { Images } from '../Themes';
@@ -13,22 +14,59 @@ export default class BappItemAdd extends Component {
   constructor(props) {
     super(props);
 
+    this.uploadImage = this.uploadImage.bind(this);
     this.submit = this.submit.bind(this);
   }
 
+  uploadImage() {
+    const options = {
+      title: 'Select photo',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else {
+        const source = { uri: response.uri };
+        console.log('source', source);
+
+        // You can also display the image using data:
+        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+      }
+    });
+  }
+
   submit() {
+    const { navigation } = this.props;
+
     // TODO: save the stuff
 
-    const { navigation } = this.props;
-    navigation.goBack();
+    // Fake timeout
+    setTimeout(() => {
+      navigation.goBack();
+    }, 1000);
   }
 
   render() {
+    const { navigation } = this.props;
+    const bapp = navigation.getParam('bapp');
+
     return (
       <SafeAreaView style={styles.mainContainer}>
-        <Text>Bapp item add</Text>
         <Image source={Images.background} style={styles.backgroundImage} resizeMode="stretch" />
+        <Text>{bapp.name}</Text>
+        <Text>Bapp item add</Text>
         <ScrollView style={styles.container}>
+          <RoundedButton onPress={this.uploadImage}>
+            Upload image
+          </RoundedButton>
           <RoundedButton onPress={this.submit}>
             Submit
           </RoundedButton>
--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { SafeAreaView, ScrollView, Image } from 'react-native';
+import {
+  SafeAreaView, ScrollView, Image, Text,
+} from 'react-native';
+import { Card, Button, Icon } from 'react-native-elements';
+
 import DevscreensButton from '../../ignite/DevScreens/DevscreensButton.js';
 import RoundedButton from '../Components/RoundedButton';
 
@@ -12,25 +16,46 @@ export default class LaunchScreen extends Component {
   constructor(props) {
     super(props);
 
-    this.openAddScreen = this.openAddScreen.bind(this);
+    this.state = {
+      bapps: [],
+    };
   }
 
-  openAddScreen() {
+  componentDidMount() {
+    fetch('http://localhost:9001/bapps/search/', {
+      method: 'GET',
+    }).then((response) => { return response.json(); })
+      .then((response) => {
+        this.setState({
+          bapps: response.data,
+        });
+      });
+  }
+
+  openAddScreen(bapp) {
     const { navigation } = this.props;
     navigation.navigate('BappItemAdd', {
-      bappId: 'bitstagram',
+      bapp,
     });
   }
 
   render() {
+    const { bapps = [] } = this.state;
+
     return (
       <SafeAreaView style={styles.mainContainer}>
         <Image source={Images.background} style={styles.backgroundImage} resizeMode="stretch" />
         <ScrollView style={styles.container}>
-          <RoundedButton onPress={this.openAddScreen}>
-            Bitstagram
-          </RoundedButton>
-
+          {bapps.map((bapp) => {
+            return (
+              <Card
+                title={bapp.name}
+                image={{ uri: bapp.definition.logo }}
+              >
+                <RoundedButton onPress={this.openAddScreen.bind(this, bapp)}>Post</RoundedButton>
+              </Card>
+            );
+          })}
           <DevscreensButton />
         </ScrollView>
       </SafeAreaView>
